e6d79a749212c1546813c857b3ef2f9451930a1f
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-02-04 10:53:34 +0100

    Add AccountSummary entity
    Add basic AccountSummary entity for a RDBMS
    
    #querymodel-replay-performance

---
+++ query-model-replay-performance-tuning/src/main/java/io/axoniq/dev/sample/query/AccountSummary.java
@@ -0,0 +1,70 @@
+package io.axoniq.dev.sample.query;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Query Model reflecting an account as an Entity.
+ *
+ * @author Steven van Beelen
+ */
+@Entity
+class AccountSummary {
+
+    @Id
+    private UUID accountId;
+    private BigDecimal balance;
+
+    public AccountSummary(UUID accountId) {
+        this.accountId = accountId;
+        this.balance = BigDecimal.ZERO;
+    }
+
+    public AccountSummary() {
+        // Default constructor
+    }
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void debitAccount(BigDecimal amount) {
+        this.balance = this.balance.subtract(amount);
+    }
+
+    public void creditAccount(BigDecimal amount) {
+        this.balance = this.balance.add(amount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AccountSummary that = (AccountSummary) o;
+        return Objects.equals(accountId, that.accountId) && Objects.equals(balance, that.balance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId, balance);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountSummary{" +
+                "accountId=" + accountId +
+                ", balance=" + balance +
+                '}';
+    }
+}
---
+++ query-model-replay-performance-tuning/src/main/java/io/axoniq/dev/sample/query/AccountSummaryRepository.java
@@ -0,0 +1,14 @@
+package io.axoniq.dev.sample.query;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+/**
+ * Repository solution for an {@link AccountSummary}.
+ *
+ * @author Steven van Beelen
+ */
+interface AccountSummaryRepository extends JpaRepository<AccountSummary, UUID> {
+
+}
