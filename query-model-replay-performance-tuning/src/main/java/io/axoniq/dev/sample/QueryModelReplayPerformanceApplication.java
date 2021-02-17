00b1097a2e8aa72bb661228d32a404c3ed507d9e
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-02-17 14:38:59 +0100

    Configure metrics
    Configure DropWizard metrics to measure the number of messages ingested
    per time unit. Log the outcome to standard out instead of to a metric
    registry, as that simplifies deployments. Lastly, make sure only the
    EventProcessor is measured, as that's all we need

--- query-model-replay-performance-tuning/src/main/java/io/axoniq/dev/sample/QueryModelReplayPerformanceApplication.java
+++ query-model-replay-performance-tuning/src/main/java/io/axoniq/dev/sample/QueryModelReplayPerformanceApplication.java
@@ -1,7 +1,21 @@
 package io.axoniq.dev.sample;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Slf4jReporter;
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.Configurer;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventhandling.EventProcessor;
+import org.axonframework.metrics.MessageTimerMonitor;
+import org.axonframework.monitoring.NoOpMessageMonitor;
+import org.axonframework.queryhandling.QueryBus;
+import org.axonframework.queryhandling.QueryUpdateEmitter;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Main application to show case how to enhance the performance of query model replays.
@@ -14,5 +28,30 @@ public class QueryModelReplayPerformanceApplication {
     public static void main(String[] args) {
         SpringApplication.run(QueryModelReplayPerformanceApplication.class, args);
     }
-    // TODO: 28-12-20 look for message monitors to measure event throughput
+
+    @Autowired
+    public void configureMessageMonitors(Configurer configurer, MetricRegistry metricRegistry) {
+        configurer.configureMessageMonitor(CommandBus.class, (c, t, n) -> NoOpMessageMonitor.INSTANCE);
+        configurer.configureMessageMonitor(EventBus.class, (c, t, n) -> NoOpMessageMonitor.INSTANCE);
+        configurer.configureMessageMonitor(QueryBus.class, (c, t, n) -> NoOpMessageMonitor.INSTANCE);
+        configurer.configureMessageMonitor(QueryUpdateEmitter.class, (c, t, n) -> NoOpMessageMonitor.INSTANCE);
+
+        configurer.configureMessageMonitor(EventProcessor.class, (config, type, name) -> {
+            MessageTimerMonitor messageTimerMonitor = new MessageTimerMonitor();
+            MetricRegistry eventProcessingRegistry = new MetricRegistry();
+            eventProcessingRegistry.register("messageTimer", messageTimerMonitor);
+            metricRegistry.register(name, eventProcessingRegistry);
+            return messageTimerMonitor;
+        });
+    }
+
+    @Autowired
+    public void configureMetricLogger(MetricRegistry metricRegistry) {
+        Slf4jReporter.forRegistry(metricRegistry)
+                     .outputTo(LoggerFactory.getLogger("io.axoniq.dev.sample.replay-performance"))
+                     .convertRatesTo(TimeUnit.SECONDS)
+                     .convertDurationsTo(TimeUnit.MILLISECONDS)
+                     .build()
+                     .start(10, TimeUnit.SECONDS);
+    }
 }
