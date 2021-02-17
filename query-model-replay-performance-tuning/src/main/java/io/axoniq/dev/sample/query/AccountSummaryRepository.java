e5b9fa3fc61f4becab25862174ce04b5c074ad18
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-02-17 11:45:29 +0100

    Fix spring profiles
    Fix spring profiles such that the loader is the sole thing being started
     when it is selected

--- query-model-replay-performance-tuning/src/main/java/io/axoniq/dev/sample/query/AccountSummaryRepository.java
+++ query-model-replay-performance-tuning/src/main/java/io/axoniq/dev/sample/query/AccountSummaryRepository.java
@@ -1,5 +1,6 @@
 package io.axoniq.dev.sample.query;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
@@ -9,6 +10,7 @@ import java.util.UUID;
  *
  * @author Steven van Beelen
  */
+@Profile("!loader")
 interface AccountSummaryRepository extends JpaRepository<AccountSummary, UUID> {
 
 }
